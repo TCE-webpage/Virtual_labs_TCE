1b59a7f86365e312a9385af8855428a18538ca32
RASHIKA-webpage <68957209+RASHIKA-webpage@users.noreply.github.com>
2022-04-24 18:54:13 +0530

    transfer and gates

--- Mosfet/simulationop.js
+++ Mosfet/simulationop.js
@@ -1,134 +1,219 @@
-// Button Display Function
-function displayfn(){
-    document.getElementById("start").style.display="none"; 
-    document.getElementById("FormNPN").style.display="inline";
-    document.getElementById("instruct").innerHTML="Click Form NPN";
-  }
-function displayfn1(){
-    document.getElementById("FormNPN").style.display="none";  
-    document.getElementById("instruct").innerHTML="Emitter is the region to the left end which supply free charge carriers i.e electrons. It is a heavily doped region."    
-    document.getElementById("img").src="simulation_gif/1)FormNPN.gif";
-    setTimeout( function() {
-      document.getElementById("SetVBE").innerHTML="BASE";
-      document.getElementById("SetVBE").style.boxShadow=' ';
-      document.getElementById("instruct").innerHTML="It is the center region. The majority carriers from the emitter region are injected into this region. This region is very thin and lightly doped.";
-     },6200);   
-    setTimeout( function() {
-      document.getElementById("SetVBE").innerHTML="COLLECTOR";
-    document.getElementById("instruct").innerHTML="It is the region to right end where charge carriers are collected. It is also heavily doped but slightly lesser than that of the emitter. The region-area of the collector is slightly more than that of the emitter.";
-     },13400); 
-     setTimeout( function() {
-      document.getElementById("SetVBE").innerHTML="DIFFUSION";
-    document.getElementById("instruct").innerHTML="During diffusion process, Depletion region at emitter and collector junction penetrate less in heavily doped emitter and collector and extends more in the base region. As collector is slightly less doped than the emitter, the depletion layer width at the collector junction is more than the depletion layer width at the emitter junction.";
-     },24400); 
-    setTimeout( function() {
-      document.getElementById("SetVBE").innerHTML="SET IB";
-      document.getElementById("SetVBE").disabled=false;
-    document.getElementById("instruct").innerHTML="Set IB as a constant by varying the VBE slider. Once it's set. Click vary VCE";
-  },36000);           
-    document.getElementById("SetVBE").style.display="inline";       
-  }
-  function displayfn2(){  
-    document.getElementById("SetVBE").style.display="none";
-    document.getElementById("VaryVCE").style.display="inline";          
-    document.getElementById("VCErange").style.display="none";
-    document.getElementsByClassName("HoriSlider")[0].style.display="none";
-    document.getElementById("verti").style.display="inline";  
-    document.getElementsByClassName("VBEName")[0].innerHTML="VBE";
-    document.getElementById("gaugeValue-demoGauge").style.display="inline";
-  }
-  function displayfn3(){
-    document.getElementById("VaryVCE").style.display="none";
-    document.getElementById("end").style.display="inline";
-    document.getElementById("gaugeValue-demoGauge").style.display="none";
-    document.getElementById("VCErange").style.display="inline";
-    document.getElementsByClassName("HoriSlider")[0].style.display="inline";
-    document.getElementsByClassName("VBEName")[0].innerHTML=" ";
-    document.getElementById("instruct").innerHTML="Vary VCE  using the slider given, add the corresponding Collector current value to the Table.Once a set of readings are taken Set another IB And repeat the same procedure"
-  }
-  
-// Formula for IB display
-function updateGauge(id){
-  var basevoltage = document.getElementById("gaugeValue-" + id).value;  
-  var Is=2*(Math.pow(10,-15));  
-  var exponent = Math.exp(basevoltage/0.026);
-  const beta=99;
-  const IB= (parseFloat((Is*exponent)/beta) * Math.pow(10,7));
-  basecurrent = IB.toFixed(3);
-  if(basecurrent > 80){
-    basecurrent = 80;
-  }
-  document.getElementById("VCElabel").innerHTML = basecurrent+" &#956;A"; 
+// Formula 
+function updateVDSlabel(min, max){
+  const newGaugeDisplayValue = document.getElementById("VGSrange").value;
+  const newGaugeValue =Math.floor(((newGaugeDisplayValue - min) / (max - min)) * 100);
+  const vdsconstval=document.getElementById("VDSrange").value;
 
-  var vceconstval = document.getElementById("VCErange").value;  
-  if(vceconstval==0 && basevoltage>0.6)
-  {
-    document.getElementById("img").src="simulation_gif/1)VCE=0-loop.gif";
-  }
-}
 
-// Displaying VCE value in the voltmeter
-function updateVCElabel(){
-  var vceconstval = document.getElementById("VCErange").value;
-  document.getElementById("Label").innerHTML = vceconstval;
-  const newGaugeValue = Math.floor(((vceconstval - 0) / (40)) * 100);
   document.getElementById("demoGauge").style.setProperty('--gauge-value', newGaugeValue);
+  document.getElementById("Label").innerHTML=newGaugeDisplayValue;
+   var vgsconstval = document.getElementById("VGSrange").value;
+    const vth = 3.0;
+    const IDon = 20;
+    const Vgon = 10;
+    const k = (IDon)/Math.pow(Vgon-vth,2);
+    var VDS = vgsconstval - vth;
+    if (vgsconstval<vth){
+      ID=0;
+    }
+    else{
+    if (vdsconstval < VDS){
+      ID =  parseFloat(2*k*(((VDS)*vdsconstval) - ((Math.pow(vdsconstval,2))/2)))
+    }
+    else{
+      ID =  parseFloat(k*Math.pow(VDS,2))
+    }
+  }
+  gauge2val=ID.toFixed(3)
+  const newGauge2Value =Math.floor(((gauge2val - 0) / (94)) * 100);
+  document.getElementById("demoGauge1").style.setProperty('--gauge-value', newGauge2Value)
 
-  // Displaying IC value in the ammeter
-  var basevoltage = document.getElementById("gaugeValue-demoGauge").value; 
-  if(basevoltage>=0.69){
-    basevoltage=0.691;
-  }
-  var Is=2*(Math.pow(10,-15));  
-  var exponent = Math.exp(basevoltage/0.026);
-  const collectorCurrent = parseFloat(Is*exponent*(1+(vceconstval/100))*100000);
-  cc = collectorCurrent.toFixed(2);
-  document.getElementById("Label1").innerHTML =cc;
-  const newGauge2Value =Math.floor(((cc - 0) / (100)) * 100);
-  document.getElementById("demoGauge1").style.setProperty('--gauge-value', newGauge2Value);
+  document.getElementById("Label1").innerHTML=gauge2val;
 
-  if(vceconstval>0 && vceconstval<=10)
+
+
+  if(vdsconstval>=0 && vdsconstval<=15 && vgsconstval==0)
   {
-    document.getElementById("img").src="simulation_gif/Slow.gif";
-  }
-  else if(vceconstval>10 && vceconstval<=20)
-  {
-    document.getElementById("img").src="simulation_gif/medium2.5.gif";
-  }
-  else if(vceconstval>20 && vceconstval<=30)
-  {
-    document.getElementById("img").src="simulation_gif/Medium1.5.gif";
-  }
-  else if(vceconstval>30 && vceconstval<=40)
-  {
-    document.getElementById("img").src="simulation_gif/Fast0.gif";
-  }
+    document.getElementById("img").src="simulation_gif/VGS0VDS15.png";
   
+  }
+  else if(vdsconstval>=15.1 && vdsconstval<=30 && vgsconstval==0)
+  {
+    document.getElementById("img").src="simulation_gif/VGS0VDS30.png";
+  
+  }
+  else if(vdsconstval>=30.1 && vdsconstval<=45 && vgsconstval==0)
+  {
+    document.getElementById("img").src="simulation_gif/VGS0VDS45.png";
+  
+  }
+  else if(vdsconstval>=45.1 && vdsconstval<=60 && vgsconstval==0)
+  {
+    document.getElementById("img").src="simulation_gif/VGS0VDS60.png";
+  
+  }
+  else if(vdsconstval>=0 && vdsconstval<=1 && vgsconstval==3.5)
+  {
+    document.getElementById("img").src="simulation_gif/VGS3.5VDS1.gif";
+  }
+  else if(vdsconstval>=0 && vdsconstval<=15 && vgsconstval>=3.6 && vgsconstval<=15)
+  {
+    document.getElementById("img").src="simulation_gif/VGS10VDS10.gif";
+  }
+  else if(vdsconstval>=15.1 && vdsconstval<=30 && vgsconstval>=3.6 && vgsconstval<=15)
+  {
+    document.getElementById("img").src="simulation_gif/VGS10VDS25.gif";
+  }
+  else if(vdsconstval>=30.1 && vdsconstval<=45 && vgsconstval>=3.6 && vgsconstval<=15)
+  {
+    document.getElementById("img").src="simulation_gif/VGS10VDS45.gif";
+  }
+  else if(vdsconstval>=45.1 && vdsconstval<=60 && vgsconstval>=3.6 && vgsconstval<=15)
+  {
+    document.getElementById("img").src="simulation_gif/VGS10VDS60.gif";
+  }
+  else if(vdsconstval>=0 && vdsconstval<=15 && vgsconstval>=15.1 && vgsconstval<=23)
+  {
+    document.getElementById("img").src="simulation_gif/VGS23VDS10.gif";
+  }
+  else if(vdsconstval>=15.1 && vdsconstval<=30 && vgsconstval>=15.1 && vgsconstval<=23)
+  {
+    document.getElementById("img").src="simulation_gif/VGS23VDS25.gif";
+  }
+  else if(vdsconstval>=30.1 && vdsconstval<=45 && vgsconstval>=15.1 && vgsconstval<=23)
+  {
+    document.getElementById("img").src="simulation_gif/VGS23VDS45.gif";
+  }
+  else if(vdsconstval>=45.1 && vdsconstval<=60 && vgsconstval>=15.1 && vgsconstval<=23)
+  {
+    document.getElementById("img").src="simulation_gif/VGS23VDS60.gif";
+  }
+  else if(vdsconstval>=0 && vdsconstval<=15 && vgsconstval>=23.1)
+  {
+    document.getElementById("img").src="simulation_gif/VGS30VDS10.gif";
+  }
+  else if(vdsconstval>=15.1 && vdsconstval<=30 && vgsconstval>=23.1)
+  {
+    document.getElementById("img").src="simulation_gif/VGS30VDS25.gif";
+  }
+  else if(vdsconstval>=30.1 && vdsconstval<=45 && vgsconstval>=23.1)
+  {
+    document.getElementById("img").src="simulation_gif/VGS30VDS45.gif";
+  }
+  else if(vdsconstval>=45.1 && vdsconstval<=60 && vgsconstval>=23.1)
+  {
+    document.getElementById("img").src="simulation_gif/VGS30VDS60.gif";
+  }
 }
 
-// Add To Table
+// GIF for VCE when VBE=0
+var flag=1;
+var flag1=1;
+function updateVGSlabel(){
+  var vdsconstval=document.getElementById("VDSrange").value;
+  var vgsconstval = document.getElementById("VGSrange").value;
+  document.getElementById("VGSlabel").innerHTML=vdsconstval+"V";
+  if(vgsconstval>=0 && vgsconstval<=0.7 && vdsconstval==0)
+  {
+    document.getElementById("img").src="simulation_gif/VGS_0_to_0.7.jpg";
+    flag=1;
+    flag1=1;
+  }
+  else if(vgsconstval>=0.8 && vgsconstval<=3 && vdsconstval==0){
+    if(flag==1){
+      document.getElementById("img").src="simulation_gif/VGSLessthanhalfVt.gif";
+      flag=0;
+    }
+    flag1=1;
+  }
+  else if(vgsconstval>=3.1 && vgsconstval<=3.5 && vdsconstval==0){
+    if(flag1==1){
+      document.getElementById("img").src="simulation_gif/VGSGreaterThanhalfVTH.gif";
+      flag1=0;}
+  }
+  if(vgsconstval>=3.6 && vdsconstval==0)
+  {
+    document.getElementById("img").src="simulation_gif/VGS3.5_to_10.jpg";
+    flag=1;
+    flag1=1;
+  }
+ 
+}
+function displayfn(){
+document.getElementById("start").style.display="none";  
+document.getElementById("FormNPN").style.display="inline";
+document.getElementById("instruct").innerHTML="Click Form NPN";
+}
+function displayfn1(){
+document.getElementById("FormNPN").style.display="none";  
+document.getElementById("instruct").innerHTML=" In Mosfet, P-Substrate is lightly doped in which holes are the majority carriers."    
+document.getElementById("img").src="simulation_gif/FormationOfNPN.gif";
+setTimeout( function() {
+document.getElementById("SetVGS").innerHTML="N Substrate";
+document.getElementById("SetVGS").style.boxShadow=' ';
+document.getElementById("instruct").innerHTML="In Mosfet, both source and drain regions are made up of N-Substrate in which the electrons are the majority carriers.";
+},7000);   
+setTimeout( function() {
+document.getElementById("SetVGS").innerHTML="Depletion Layer";
+document.getElementById("instruct").innerHTML="There is a formation of depletion layer between the P-Substrate and N-Substrates.";
+},20200); 
+setTimeout( function() {
+document.getElementById("SetVGS").innerHTML="SiO<sub>2</sub>";
+document.getElementById("instruct").innerHTML="SiO<sub>2</sub> above the P-Substrate acts as an insulator.";
+},29100); 
+setTimeout( function() {
+document.getElementById("SetVGS").innerHTML="Circuit Connection";
+// document.getElementById("SetVGS").disabled=false;
+document.getElementById("instruct").innerHTML="Once the NPN is formed, circuit connection is given and you can see that gate terminal is insulated from P-Substrate by SiO<sub>2</sub> to avoid gate current.";
+},36000);
+setTimeout( function() {
+document.getElementById("SetVGS").innerHTML="SET VDS";
+document.getElementById("SetVGS").disabled=false;
+document.getElementById("instruct").innerHTML="Click the SET VGS button.";
+},51000);           
+document.getElementById("SetVGS").style.display="inline";       
+}
+function displayfn2(){  
+document.getElementById("SetVGS").style.display="none";
+document.getElementById("VaryVDS").style.display="inline";          
+document.getElementById("VDSrange").style.display="inline"
+document.getElementsByClassName("HoriSlider1")[0].style.display="inline";
+document.getElementById("instruct").innerHTML="Vary VGS using upper horizontal slider,You can observe that there is a formation of n-channel between source and drain only after VGS greater than 3V(Threshold Voltage).<br>Now Set VGS as a constant voltage.";
+
+}
+function displayfn3(){
+document.getElementById("VaryVDS").style.display="none";
+document.getElementById("end").style.display="inline";
+document.getElementById("VDSrange").style.display="none";
+document.getElementsByClassName("HoriSlider1")[0].style.display="none";
+document.getElementById("VGSrange").style.display="inline";
+document.getElementsByClassName("HoriSlider2")[0].style.display="inline"; 
+document.getElementById("instruct").innerHTML="Vary VDS  using the slider given, add the corresponding Drain current value to the Table.Once a set of readings are taken Set another VGS And repeat the same procedure"
+}
+
 // Table Creation Code JS
 var tabrowindex = 0;
 var xArray=[0];
 var yArray=[0];
 var i=1;
 function tabled(){
-
-document.getElementById("tbvce1").innerHTML = basecurrent +" &#956;A";
+var vgs = document.getElementById("VGSrange").value;
+var vds = document.getElementById("VDSrange").value;
+document.getElementById("tbvgs1").innerHTML = vds+" V";
 
 var sno = ++tabrowindex;
-var voltce = document.getElementById("VCErange").value;
-var ic = cc;
+// var vds = document.getElementById("VDSrange").value;
+var id = gauge2val;
 var table = document.getElementById("mytable");
 var row = table.insertRow(-1);
 var cell1 = row.insertCell(0);
 var cell2 = row.insertCell(1);
 var cell3 = row.insertCell(2);
 cell1.innerHTML = sno;
-cell2.innerHTML = voltce;
-cell3.innerHTML = ic;   
-xArray[i]= voltce;
-yArray[i]= ic;
+cell2.innerHTML = vgs;
+cell3.innerHTML = id;   
+xArray[i]=vgs;
+yArray[i]=id;
 i+=1;    
 }
 function deleted(){
@@ -139,56 +224,102 @@ i--;
 }
 }
 function displayfn4(){
-  document.getElementById("end").style.display="inline";
-  document.getElementById("VCErange").style.display="inline";
-  document.getElementsByClassName("VBEName")[0].innerHTML="VBE";
-  document.getElementById("gaugeValue-demoGauge").style.display="inline";
-  document.getElementsByClassName("HoriSlider")[0].style.display="inline";
-  // document.getElementById("gaugeValue-demoGauge").value=0;
-  document.getElementById("VCErange").value=0;
-  document.getElementById("img").src="simulation_gif/pic2.png";
-  while(tabrowindex){
-  document.getElementById("mytable").deleteRow(-1);
-  tabrowindex--;
+var vdsconstval= 0;
+flag = 1;
+flag1=1;
+var vgsconstval = document.getElementById("VGSrange").value;
+document.getElementById("end").style.display="inline";
+document.getElementById("VGSrange").style.display="inline"
+document.getElementsByClassName("HoriSlider2")[0].style.display="inline";
+document.getElementById("VDSrange").style.display="inline";
+document.getElementsByClassName("HoriSlider1")[0].style.display="inline";
+document.getElementById("VDSrange").value=0;
+const newGaugeValue =0;
+document.getElementById("demoGauge").style.setProperty('--gauge-value', newGaugeValue);
+document.getElementById("Label").innerHTML= 0.00;
+
+const newGauge2Value =0;
+document.getElementById("demoGauge1").style.setProperty('--gauge-value', newGauge2Value);
+
+document.getElementById("Label1").innerHTML= 0.00;
+if(vgsconstval>=0 && vgsconstval<=0.7 && vdsconstval==0)
+  {
+    document.getElementById("img").src="simulation_gif/VGS_0_to_0.7.jpg";
+    flag=1;
+    flag1=1;
   }
-  document.getElementById("tbvce1").innerHTML = " ";       
-   xArray=[0];
-   yArray=[0];
-   i=1; 
-   
-  // Define Data
-  var data = [{
-   x: xArray,
-   y: yArray,
-   mode:"lines"
-   }];
-
-   // Define Layout
-   var layout = {
-   xaxis: {range: [0, 40], title: "Collector-Emitter Voltage (V)"},
-   yaxis: {range: [0, 100], title: "Collector Current (mA)"},  
-   title: "Output Characteristics - VCE Vs IC"
-  };
-
-   // Display using Plotly
-     Plotly.newPlot("myPlot", data, layout);
+  else if(vgsconstval>=0.8 && vgsconstval<=2 && vdsconstval==0){
+    if(flag==1){
+      document.getElementById("img").src="simulation_gif/VGSLessthanhalfVt.gif";
+      flag=0;
+    }
+    flag1=1;
+  }
+  else if(vgsconstval>=2.1 && vgsconstval<=3.5 && vdsconstval==0){
+    if(flag1==1){
+      document.getElementById("img").src="simulation_gif/VGSGreaterThanhalfVTH.gif";
+      flag1=0;}
+  }
+  if(vgsconstval>=3.6 && vgsconstval<=10 && vdsconstval==0)
+  {
+    document.getElementById("img").src="simulation_gif/VGS3.5_to_10.jpg";
+    flag=1;
+    flag1=1;
+  }
+  if(vgsconstval>=10.1 && vgsconstval<=20 && vdsconstval==0)
+  {
+    document.getElementById("img").src="simulation_gif/VGS10.1_to_20.jpg";
+    flag=1;
+    flag1=1;
+  }
+  else if(vgsconstval>=20.1 && vgsconstval<=30 && vdsconstval==0)
+  {
+    document.getElementById("img").src="simulation_gif/VGS20.1_to_30.jpg";
+    flag=1;
+    flag1=1;
+  }
+console.log('sd')
+while(tabrowindex){
+document.getElementById("mytable").deleteRow(-1);
+tabrowindex--;
 }
-function plotgraph1(){
+document.getElementById("tbvgs1").innerHTML = " ";       
+xArray=[0];
+yArray=[0];
+i=1; 
+
 // Define Data
- var data = [{
-   x: xArray,
-   y: yArray,
-   mode:"lines"
-   }];
+var data = [{
+x: xArray,
+y: yArray,
+mode:"lines"
+}];
 
 // Define Layout
 var layout = {
-xaxis: {range: [0, 40], title: "Collector-Emitter Voltage (V)"},
-yaxis: {range: [0, 100], title: "Collector Current (mA)"},  
-title: "Output Characteristics - VCE Vs IC"
+  xaxis: {range: [0, 30], title: "Gate-Source Voltage (V)"},
+  yaxis: {range: [0, 300], title: "Gate Current (A)"},  
+  title: "Transfer Characteristics - VGS Vs IG"
+  };
+
+// Display using Plotly
+  Plotly.newPlot("myPlot", data, layout);
+}
+function plotgraph(){
+// Define Data
+var data = [{
+x: xArray,
+y: yArray,
+mode:"lines"
+}];
+
+// Define Layout
+var layout = {
+xaxis: {range: [0, 30], title: "Gate-Source Voltage (V)"},
+yaxis: {range: [0, 300], title: "Gate Current (A)"},  
+title: "Transfer Characteristics - VGS Vs IG"
 };
 
 // Display using Plotly
 Plotly.newPlot("myPlot", data, layout);
 }
-
